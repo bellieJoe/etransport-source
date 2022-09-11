5be7e19ec8279a47b21a93e56ff3a50a3cbf1fbf
Bellie Joe Jandusay <jandusayjoe14@gmail.com>
2022-09-11 19:31:53 +0800

    asd

---
+++ e-transport-backend/app/Models/ServiceType.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class ServiceType extends Model
+{
+    use HasFactory;
+
+    protected $primaryKey = 'service_type_id';
+
+    protected $fillable = [
+        'service_description'
+    ];
+}
