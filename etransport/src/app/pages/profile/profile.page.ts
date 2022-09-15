add1b0fc407c853852845e397a6bf48569e2b3b1
Bellie Joe Jandusay <jandusayjoe14@gmail.com>
2022-09-15 19:43:03 +0800

    Implemented the profile feature

--- etransport/src/app/pages/profile/profile.page.ts
+++ etransport/src/app/pages/profile/profile.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
   selector: 'app-profile',
@@ -7,9 +8,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProfilePage implements OnInit {
 
-  constructor() { }
+  constructor(
+    private authService : AuthService
+  ) { }
+
+  user : any = {}
+  role = {}
 
   ngOnInit() {
+  }
+
+  ionViewWillEnter(){
+    this.user = this.authService.getAuth();
+    this.role = this.authService.getAuth().role;
+    console.log(this.user.role.role_description)
   }
 
 }
--- etransport/src/app/services/user.service.ts
+++ etransport/src/app/services/user.service.ts
@@ -81,4 +81,5 @@ export class UserService {
 
     this.router.navigate(['/signin'])
   }
+
 }
