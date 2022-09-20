48257c74982c267dccdb0b66e99c63e6b5c8198c
Bellie Joe <jandusayjoe14@gmail.com>
2022-09-20 09:47:26 +0800

    Added lisitngs

--- etransport/src/app/pages/listings/listings.page.ts
+++ etransport/src/app/pages/listings/listings.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AlertController } from '@ionic/angular';
 import { ServiceService } from 'src/app/services/service.service';
 
 @Component({
@@ -9,13 +10,29 @@ import { ServiceService } from 'src/app/services/service.service';
 export class ListingsPage implements OnInit {
 
   constructor(
-    public serviceService : ServiceService
+    public serviceService : ServiceService,
+    private alertController : AlertController
   ) { }
 
+  loading : boolean = false;
 
   async fetchListings(){
+    this.loading = true;
     const res = await this.serviceService.getListings();
+
+    if(res.status != 200){
+      const alert = await this.alertController.create({
+        header: "Error fetching services",
+        message: `${res.status} | ${res.data.message}`,
+        buttons: ['Ok']
+      });
+      await alert.present();
+      this.loading = false;
+      return;
+    }
+
     this.serviceService.listings = res.data;
+    this.loading = false;
   }
 
   async ngOnInit() {
