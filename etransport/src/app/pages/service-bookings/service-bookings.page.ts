cd262a4371b64175ba225687582aa31ecc37e16c
Bellie Joe <jandusayjoe14@gmail.com>
2022-10-03 14:32:44 +0800

    Finish implementing the decline booking featuuuure

--- etransport/src/app/pages/service-bookings/service-bookings.page.ts
+++ etransport/src/app/pages/service-bookings/service-bookings.page.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
+import { AfterViewInit, Component, OnInit, QueryList, ViewChild, ViewChildren } from '@angular/core';
 import { AlertController, IonModal, LoadingController } from '@ionic/angular';
 import { AuthService } from 'src/app/services/auth.service';
 import { ServiceService } from 'src/app/services/service.service';
@@ -19,10 +19,29 @@ export class ServiceBookingsPage implements OnInit {
     private loadingController : LoadingController
   ) { }
 
+  @ViewChildren(IonModal) ionModals : QueryList<IonModal>;
+
   filteredBooking : string = 'all';
   isLoading : boolean = false;
   transport = [];
   msg_from_admin: null;
+
+  sampleClose(){
+    this.closeModals()
+  }
+  closeModals(){
+    this.ionModals.map(async (ionModal : IonModal) => {
+      await ionModal.dismiss();
+    });
+    // await new Promise((resolve => {
+    //   this.ionModals.forEach(async(ionModal) => {
+    //     await ionModal.dismiss();
+    //     if(this.ionModals[this.ionModals.length - 1].trigger === ionModal.trigger){
+    //       resolve(null);
+    //     }
+    //   })
+    // }))
+  }
 
   setBookingStatusColor(status : string){
     if(status == 'accepted' || status == 'finished'){
@@ -101,14 +120,73 @@ export class ServiceBookingsPage implements OnInit {
       ]
     });
     await alert.present();
+    await alert.onDidDismiss();
+    this.closeModals();
   }
+
+  async declineBooking(transport_booking_id: any){
+    const confirmHandler = async () => {
+      const loader = await this.loadingController.create({
+        backdropDismiss: false,
+        message: "Accepting booking.",
+        spinner: "circular"
+      });
+      await loader.present();
+      const data : UpdateStatusData = {
+        booking_status: 'declined',
+        message: 'Service booking has been declined.',
+        transport_booking_id: transport_booking_id,
+        msg_from_admin: this.msg_from_admin,
+        msg_from_customer: null
+      };
+      const res = await this.transportBookingService.updateStatus(data)
+      if(res.status != 200){
+        const alert = await this.alertController.create({
+          header: `Unexpected Error`,
+          message: `${ res.status } | ${ res.data.message }`,
+          buttons: ["Ok"]
+        });
+        await loader.dismiss();
+        await alert.present();
+        return;
+      }
+      this.transportBookingService.transport_bookings.every((val, i)=>{
+        if(val.transport_booking_id == res.data.transport_booking_id){
+          this.transportBookingService.transport_bookings[i] = res.data;
+          return false;
+        }
+        return true;
+      })
+      await loader.dismiss();
+    }
+
+    const alert = await this.alertController.create({
+      header: 'Confirm action',
+      message: 'Are you sure you want to decline this booking?',
+      buttons: [
+        {
+          text: "Cancel",
+          role: "cancel"
+        },
+        {
+          text: "Ok",
+          role: 'ok',
+          handler: confirmHandler
+        }
+      ]
+    });
+    await alert.present();
+    await alert.onDidDismiss();
+    this.closeModals();
+  }
+
 
   async ngOnInit() {
     this.isLoading = true;
     await this.serviceService.fetchServiceByUserId();
     await this.fetchBookings();
     this.isLoading = false;
-    this.transport = this.transportBookingService.transport_bookings
-    console.log(this.transportBookingService.transport_bookings)
+    this.transport = this.transportBookingService.transport_bookings;
+    // await this.closeModals();
   }
 }
