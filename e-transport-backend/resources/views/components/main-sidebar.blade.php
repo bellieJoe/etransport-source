341db556e00b894c3f62b62c9881abe2d8e6c486
Bellie Joe Jandusay <jandusayjoe14@gmail.com>
2022-10-11 21:59:54 +0800

    Implemented the AdminLTE Dashboard UI to Etransport Admin

---
+++ e-transport-backend/resources/views/components/main-sidebar.blade.php
@@ -0,0 +1,32 @@
+<aside class="main-sidebar sidebar-dark-primary elevation-4 vh-100">
+    <div class="brand-link d-flex justify-content-between align-items-center">
+        <a class="brand-link" href="index3.html">
+            ETransport Admin
+        </a>
+    </div>
+    <!-- Sidebar -->
+    <div class="sidebar ">
+        <!-- Sidebar user panel (optional) -->
+        <div class="user-panel mt-3 pb-3 mb-3 d-flex">
+            <div class="info">
+                <a href="#" class="d-block">Name of Admin</a>
+            </div>
+        </div>
+        <!-- Sidebar Menu -->
+        <nav class="mt-2 ">
+            <ul class="nav nav-pills nav-sidebar flex-column" >
+                <li class="nav-item ">
+                    <a href="#" class="nav-link active">
+                        <i class="nav-icon fas fa-th"></i>
+                        <p>Simple Link</p>
+                    </a>
+                </li>
+            </ul>
+        </nav>
+        <!-- /.sidebar-menu -->
+    </div>
+</aside>
+
+<script>
+
+</script>
--- e-transport-backend/resources/views/index.blade.php
+++ e-transport-backend/resources/views/index.blade.php
@@ -1,48 +1,46 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
 
-     {{-- Main CSS --}}
+    {{-- Main CSS --}}
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 
     {{-- Bootstrap icons --}}
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.9.1/font/bootstrap-icons.css">
 
-    {{-- Bootstrap 5 CSS --}}
+    {{-- BS 5 CSS --}}
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+
+    {{-- adminlte --}}
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/admin-lte@3.2/dist/css/adminlte.min.css">
 
     <!-- fontawesome -->
     <script src="https://kit.fontawesome.com/2a90b2a25f.js" crossorigin="anonymous"></script>
 
+    {{-- JQuery --}}
+    <script src="https://code.jquery.com/jquery-3.6.1.min.js" integrity="sha256-o88AwQnZB+VDvE9tvIXrMQaPlFFSUTR+nldQm1LuPXQ=" crossorigin="anonymous"></script>
+
     <title>ETransport Admin</title>
 </head>
+
 <body>
-    <section class="row g-0">
-        <nav class="col-auto bg-dark vh-100 ">
-            <div class="mb-3 p-3 border-b border-gray-500">
-                <a class="navbar-brand text-light fw-bold fs-4" href="/">ETransport</a>
-            </div>
-            <ul class="nav nav-pills flex-column nav-fill px-3 ">
-                <li class="nav-item ">
-                    <a href="#" class="nav-link active text-light text-left"><i class="bi bi-speedometer2 me-2"></i> Dashboard</a>
-                </li>
-                {{-- <li class="nav-item ">
-                    <a href="#" class="nav-link  text-light text-left"><i class="bi bi-people me-2"></i> Administrators</a>
-                </li> --}}
-                <li class="nav-item ">
-                    <button  class="nav-link text-light text-left"><i class="bi bi-people me-2"></i> Administrators <i class="fas fa-caret-left text-gray-500 ms-2"></i></button>
-                </li>
-            </ul>
-        </nav>  
-        <div class="col">
+    <div class="wrapper">
+        @include('components.main-header')
+        @include('components.main-sidebar')
+        <div class="content-wrapper bg-gray-200">this is the content</div>
+    </div>
 
-        </div>
-    </section>
-
-    {{-- Bootstrap 5 JS --}}
+    {{-- BS 5 JS --}}
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
+
+    {{-- adminlte --}}
+    <script src="https://cdn.jsdelivr.net/npm/admin-lte@3.2/dist/js/adminlte.min.js"></script>
+
+    
 </body>
+
 </html>
