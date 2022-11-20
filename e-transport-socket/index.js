4c6867b575d239bd1d074efa4cb33cec2580c8a0
Bellie Joe Jandusay <jandusayjoe14@gmail.com>
2022-11-20 13:43:41 +0800

    added socket server

---
+++ e-transport-socket/index.js
@@ -0,0 +1,22 @@
+
+const app = require('express')();
+const httpServer = require('http').createServer(app);
+const io = require('socket.io')(httpServer, {
+  cors: {origin : '*'}
+});
+
+const port = process.env.PORT || 3000;
+
+io.on('connection', (socket) => {
+  console.log('a user connected');
+  
+  socket.on('message', (message) => {
+    io.emit('message', message);
+  });
+
+  socket.on('disconnect', () => {
+    console.log('a user disconnected!');
+  });
+});
+
+httpServer.listen(port, () => console.log(`listening on port ${port}`));
