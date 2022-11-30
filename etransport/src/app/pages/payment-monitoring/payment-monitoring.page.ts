69353d23ee54feaa2d199e3a29eeafddfa3d35b7
Bellie Joe Jandusay <jandusayjoe14@gmail.com>
2022-11-30 22:35:37 +0800

    Implement ionic infinite scroll and refresher to administrator payments page

--- etransport/src/app/pages/payment-monitoring/payment-monitoring.page.ts
+++ etransport/src/app/pages/payment-monitoring/payment-monitoring.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { InfiniteScrollCustomEvent } from '@ionic/angular';
 import { AuthService } from 'src/app/services/auth.service';
 import { PaymentService } from 'src/app/services/payment.service';
 import { ServiceService } from 'src/app/services/service.service';
@@ -19,15 +20,36 @@ export class PaymentMonitoringPage implements OnInit {
   payments : any = {};
   filteredPayment : string = 'all';
   loading : boolean = false;
+  page : number = 1;
 
   async ngOnInit() {
     await this.getPayments();
   }
 
+  async loadMore(ev){
+    const payments : any = await this.paymentService.getPaymentsByUserId(
+      this.authService.getAuth().user_id, 
+      {
+        status: this.filteredPayment,
+        page: this.page
+      }
+    );
+    payments.data.map((payment, i)=>{
+      this.payments.data.push(payment);
+    });
+    this.page++;
+    (ev as InfiniteScrollCustomEvent).target.complete();
+  }
+
+  async doRefresh(ev){
+    await this.getPayments();
+    ev.target.complete();
+  }
+
 
   async getPayments(){
-    console.log(this.filteredPayment);   
     this.loading = true;
+    this.page = 1;
     await this.serviceService.fetchServiceByUserId();
     this.payments = await this.paymentService.getPaymentsByServiceId(
       this.serviceService.service.service_id, 
@@ -37,6 +59,7 @@ export class PaymentMonitoringPage implements OnInit {
     );
     console.log(this.payments);
     this.loading = false;
+    this.page++;
   }
 
 
